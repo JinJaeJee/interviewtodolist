1c1af2add90f2dc52a78da2de0fe0d57e46518e0
JinJaeJee <jpluemphiromnat@gmail.com>
2023-11-24 15:51:30 +0700

    Feat: add delete and post feature

--- src/Todolist.js
+++ src/Todolist.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Todolist = () => {
   const [Todo, setTodo] = useState([]);
+  const [NewItem, setNewItem] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3001/todos")
@@ -13,19 +14,52 @@ const Todolist = () => {
       .catch((error) => console.error("Error fetching todos", error));
   }, []);
 
-  console.log(Todo);
+  const handleAddTodo = () => {
+    if (NewItem) {
+      fetch("http://localhost:3001/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: NewItem }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setTodo([...Todo, data]);
+          setNewItem("");
+        })
+        .catch((error) => console.error("Error adding todo", error));
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    fetch(`http://localhost:3001/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        const updatedTodos = Todo.filter((todo) => todo.id !== id);
+        setTodo(updatedTodos);
+      })
+      .catch((error) => console.error("Error deleting todo", error));
+  };
 
   return (
     <div>
       <h1>Todo List</h1>
-      <input type="text" />
-      <button>Add Todo</button>
+      <input
+        type="text"
+        value={NewItem}
+        onChange={(event) => setNewItem(event.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <ul>
         {Todo.map((list) => (
-          <li>{list.title}</li>
+          <li key={list.id}>
+            {list.title}
+            <button onClick={() => handleDeleteTodo(list.id)}>Delete</button>
+          </li>
         ))}
-        <button>delete</button>
-        <button>Edit</button>
       </ul>
     </div>
   );
